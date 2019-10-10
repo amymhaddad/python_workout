5d8123cafd844966e775fae1eaaf3f230bdc3fcd
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-10 10:39:30 -0400

    2 of 3 exercises are complete

--- ch_2/bigger_sum/README.md
+++ ch_2/bigger_sum/README.md
@@ -1,1 +1,13 @@
+This file contains three exercises:
+
+
+Exercise 1:
 Write a function, mysum_bigger_than, that takes a first argument, preceding \*args. That argument indicates the threshold for including an argument in the sum. Thus, calling mysum_bigger_than(10, 5, 20, 30, 6) would return 50 — because 5 and 6 aren’t greater than 10. This function should similarly work with any types, and assumes that all of the arguments are of the same type. Note that > and < work on many different types in Python, not just on numbers; with strings, lists, and tuples, it refers to their sort order.
+
+
+Exercise 2:
+Write a function, sum_numeric, which takes any number of arguments. If the argument is or can be turned into an integer, then it should be added to the total. Those arguments which cannot be handled as integers should be ignored. The result is the sum of the numbers. Thus, sum_numeric(10, 20, 'a', '30', 'bcd') would return 60. Notice that even if the string 30 is an element in the list, it’s converted into an integer and added to the total.
+
+
+Exercise 3:
+Write a function that takes a list of dictionaries, and returns a single dictionary that combines all of the keys and values. If a key appears in more than one argument, then the value should be a list containing all of the values from the arguments.
--- ch_2/bigger_sum/bigger_sum.py
+++ ch_2/bigger_sum/bigger_sum.py
@@ -1,10 +1,5 @@
 
-
-#Write a function, mysum_bigger_than, that takes a first argument, preceding \*args. 
-# That argument indicates the threshold for including an argument in the sum. 
-# Thus, calling mysum_bigger_than(10, 5, 20, 30, 6) would return 50 — because 5 and 6 aren’t greater than 10. 
-# This function should similarly work with any types, and assumes that all of the arguments are of the same type.  
-
+#Exercise 1
 def mysum_bigger_than(arg1, *args):
     threshold = arg1
     total = type(args[0])()
@@ -19,3 +14,46 @@ def mysum_bigger_than(arg1, *args):
 
 # print(mysum_bigger_than('ttt', 'bbb', 'xxw'))
 
+
+#Exercise 2:
+from string import digits
+
+def sum_numeric(*args):
+
+    total = 0
+    for item in args:
+        if isinstance(item, int):
+            total += item
+        elif item in digits:
+            total += int(item)
+    return total
+
+# print(sum_numeric(10, 20, 'a', '30', 'bcd'))
+
+
+
+#Exercise 3:
+
+people = [{'first':'Reuven', 'last':'Lerner', 'email':'reuven@lerner.co.il'},
+ {'first':'Donald', 'last':'Trump', 'email':'president@whitehouse.gov'},
+ {'first':'Vladimir', 'last':'Putin', 'email':'president@kremvax.ru'}
+ ]
+
+
+def single_dictionary(people):
+
+    single_dictionary = {}
+    
+    for person in people:
+        for key, value in person.items():
+            # single_dictionary.get(key, value)
+            # single_dictionary[key] = value
+    return single_dictionary
+        
+print(single_dictionary(people))
+
+
+    # for key, value in person.items():
+    #     # print(key, value)
+    #     new_dictionary['key'] = value
+        
