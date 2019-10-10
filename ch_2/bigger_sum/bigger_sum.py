037c563c8b3ebbd1eef314372583d021ff7dadee
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-10 10:04:47 -0400

    Final changes to bigger sum - first ex

--- ch_2/bigger_sum/bigger_sum.py
+++ ch_2/bigger_sum/bigger_sum.py
@@ -1,24 +1,21 @@
 
 
-
-
+#Write a function, mysum_bigger_than, that takes a first argument, preceding \*args. 
+# That argument indicates the threshold for including an argument in the sum. 
+# Thus, calling mysum_bigger_than(10, 5, 20, 30, 6) would return 50 — because 5 and 6 aren’t greater than 10. 
+# This function should similarly work with any types, and assumes that all of the arguments are of the same type.  
 
 def mysum_bigger_than(arg1, *args):
-
     threshold = arg1
     total = type(args[0])()
 
     for items in args:
-        
-        if isinstance(total, int):
-            
-            if items >= threshold:
-                # import pdb; pdb.set_trace()
-                total += items
-        # else:
+        if items >= threshold:
+            total += items
 
     return total
 
-print(mysum_bigger_than(10, 5, 20, 30, 6))
+# print(mysum_bigger_than(10, 5, 20, 30, 6))
 
-# print(mysum_bigger_than('azz', 'bbb', 'ccc'))
+# print(mysum_bigger_than('ttt', 'bbb', 'xxw'))
+
