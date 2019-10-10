3ff7e0b91a2f3f8645980905367b45d3d428345e
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-10 09:54:50 -0400

    refactored function to include isinstance()

--- ch_2/bigger_sum/bigger_sum.py
+++ ch_2/bigger_sum/bigger_sum.py
@@ -8,10 +8,17 @@ def mysum_bigger_than(arg1, *args):
     threshold = arg1
     total = type(args[0])()
 
-    for item in args:
-        if item >= threshold:
-            # import pdb; pdb.set_trace()
-            total += item
+    for items in args:
+        
+        if isinstance(total, int):
+            
+            if items >= threshold:
+                # import pdb; pdb.set_trace()
+                total += items
+        # else:
+
     return total
 
 print(mysum_bigger_than(10, 5, 20, 30, 6))
+
+# print(mysum_bigger_than('azz', 'bbb', 'ccc'))
