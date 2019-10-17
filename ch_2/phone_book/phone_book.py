f6bfdc85afe1d26c58b70b86f583874faeb4011c
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-17 10:50:37 -0400

    Final version of phonebook

--- ch_2/phone_book/phone_book.py
+++ ch_2/phone_book/phone_book.py
@@ -1,44 +1,35 @@
-people = [{'first':'Reuven', 'last':'Lerner', 'email':'reuven@lerner.co.il'},
- {'first':'Donald', 'last':'Trump', 'email':'president@whitehouse.gov'},
- {'first':'Vladimir', 'last':'Putin', 'email':'president@kremvax.ru'}
+people = [
+    {"first": "Reuven", "last": "Lerner", "email": "reuven@lerner.co.il"},
+    {"first": "Donald", "last": "Trump", "email": "president@whitehouse.gov"},
+    {"first": "Vladimir", "last": "Putin", "email": "president@kremvax.ru"},
 ]
 
-import operator
+
+def update_people(people):
+    """Put a dictionary into a list"""
+    updated_list = []
+    for person in people:
+        attribute = sorted(person, reverse=True)
+        for item in attribute:
+            updated_list.append(person[item])
+    return updated_list
 
 
-
-updated_list = []
-
-for person in people:
-    attribute = sorted(person, reverse=True)
-    for item in attribute:
-        updated_list += [person[item]]
-    
-print(updated_list)
+people_list = update_people(people)
 
 
-# student_tuples = [
-#     ('john', 'A', 15),
-#     ('jane', 'B', 12),
-#     ('dave', 'B', 10),
-# ]
-# # sorted(student_tuples, key=lambda student: student[2])   # sort by age
-# # [('dave', 'B', 10), ('jane', 'B', 12), ('john', 'A', 15)]
+def alpha_order(people_list):
+    """Put a given list in alphabetical order: Lastname, Firstname, Email"""
 
-# # print(sorted(people, key=lambda lastname: lastname[1]))
-
-new = [
-    ['Lerner', 'Reuven', 'reuven@lerner.co.il'],
-    ['Trump', 'Donald', 'president@whitehouse.gov'],
-    ['Putin', 'Vladimir', 'president@kremvax.ru'],
-]
-
-# x = sorted(new, key=lambda x:x[1])
-
-# for person in x:
-#     print(" ".join(person))
+    index = 0
+    final = []
+    for person in people_list:
+        final.append(people_list[index : index + 3])
+        index += 3
+        if index >= len(people_list):
+            return final
+            break
 
 
-
-
-
+for person in alpha_order(people_list):
+    print(" ".join(person))
