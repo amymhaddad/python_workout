f484c58c3de57667800c3f73c61f57952a03164c
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-17 10:11:18 -0400

    Got basic algorithm

--- ch_2/phone_book/phone_book.py
+++ ch_2/phone_book/phone_book.py
@@ -6,31 +6,39 @@ people = [{'first':'Reuven', 'last':'Lerner', 'email':'reuven@lerner.co.il'},
 import operator
 
 
-combined = {}
+
+updated_list = []
 
 for person in people:
-    for attribute, details in person.items():
-        if attribute not in combined:
-            combined[attribute] = [details]
-        else:
-            combined[attribute].append(details)
+    attribute = sorted(person, reverse=True)
+    for item in attribute:
+        updated_list += [person[item]]
+    
+print(updated_list)
 
 
+# student_tuples = [
+#     ('john', 'A', 15),
+#     ('jane', 'B', 12),
+#     ('dave', 'B', 10),
+# ]
+# # sorted(student_tuples, key=lambda student: student[2])   # sort by age
+# # [('dave', 'B', 10), ('jane', 'B', 12), ('john', 'A', 15)]
 
-student_tuples = [
-    ('john', 'A', 15),
-    ('jane', 'B', 12),
-    ('dave', 'B', 10),
+# # print(sorted(people, key=lambda lastname: lastname[1]))
+
+new = [
+    ['Lerner', 'Reuven', 'reuven@lerner.co.il'],
+    ['Trump', 'Donald', 'president@whitehouse.gov'],
+    ['Putin', 'Vladimir', 'president@kremvax.ru'],
 ]
-# sorted(student_tuples, key=lambda student: student[2])   # sort by age
-# [('dave', 'B', 10), ('jane', 'B', 12), ('john', 'A', 15)]
 
-# print(sorted(people, key=lambda lastname: lastname[1]))
+# x = sorted(new, key=lambda x:x[1])
 
-new = {
-    'person1': ['Reuven', 'Lerner', 'reuven@lerner.co.il'],
-    'person2': ['Donald', 'Trump', 'president@whitehouse.gov'],
-}
+# for person in x:
+#     print(" ".join(person))
 
-person1 = ['Reuven', 'Lerner', 'reuven@lerner.co.il']
+
+
+
 
