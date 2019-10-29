bfd7d1a7d6c7d4bfc24ece6d53f90a7c40a36e1c
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-29 10:20:54 -0400

    Final updates to repeating vowels for PR

--- ch_2/repeating_letters/repeating_vowels.py
+++ ch_2/repeating_letters/repeating_vowels.py
@@ -1,29 +1,27 @@
+"""Find the word with the greatest number of repeated vowels."""
 
 from collections import Counter
-import operator
 
-words = ['ttttttttttest', 'program', 'iiiiis']
+words = ["ttttttttttest", "program", "iiiiis"]
 vowels = "aeiou"
 
 
+# Option 1
+repeated_vowels = {
+    word: max(Counter(word).items(), key=lambda pair: pair[1]) for word in words
+}
+print(max(repeated_vowels, key=lambda w: repeated_vowels[w][0] in vowels))
 
-#need to get a conditional in here somehow to only count the letters with vowels    
-repeated_vowels = { word: max(Counter(word).items(), key=lambda pair:pair[1]) for word in words}
 
-print(max(repeated_vowels, key=lambda w:repeated_vowels[w][0] in vowels))
-
-
+# Option 2
 def most_repeating_letter_count(word):
     for letter in word:
         return Counter(word).most_common()
 
 
 def most_vowels(words):
-    repeats = { word: most_repeating_letter_count(word) for word in words}
-    # print(repeats)
+    repeats = {word: most_repeating_letter_count(word) for word in words}
+    return max(repeats, key=lambda x: x[0] in vowels)
 
-    return max(repeats, key=lambda x: x in vowels)
 
-# print(most_vowels(words))
-
-# return max(word_counts.items(), key=operator.itemgetter(1))[0]
+print(most_vowels(words))
